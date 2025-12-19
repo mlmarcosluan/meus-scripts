52593d8705dfe77aad906f0acb35d89bfc25cf1b
Marcos luan <mlmarcosluan@gmail.com>
2025-12-18 22:54:18 -0300

    Adicionado o icone do spotify na saida para o genmon.

--- genmon/my-music-spotify/my-music-spotify.sh
+++ genmon/my-music-spotify/my-music-spotify.sh
@@ -20,14 +20,20 @@ main() {
     # Variáveis locais
     local status_atual
     local info
+    local icon_path
+
+    # Icone do spotfy
+    icon_path="/usr/share/icons/hicolor/24x24/apps/spotify-client.png"
     
     status_atual=$(get_spotify_status)
     # Se o status do spotify for diferente de "Em Reprodução" entra no segundo if, caso contrario sai do primeiro if sem fazer nada
     if [[ "$status_atual" != "Em repordução" ]]; then
-        if [[ "$status_atual" == "Pausado" ]]; then 
+        if [[ "$status_atual" == "Pausado" ]]; then
+            echo "<img>$icon_path</img>"
             echo "<txtclick>playerctl -p spotify play</txtclick><txt>$status_atual</txt>"
             echo "<tool>Spotify $status_atual</tool>"
         else # Para outras saidas, como a "Fechado"
+            echo "<img>$icon_path</img>"
             echo "<txtclick>spotify</txtclick><txt>Spotify fechado</txt>"
         fi
         return
@@ -36,6 +42,7 @@ main() {
     info=$(playerctl -p spotify metadata --format "{{title}} - {{artist}}")
 
     # Saida para o genmon
+    echo "<img>$icon_path</img>"
     echo "<txtclick>playerctl -p spotify pause</txtclick><txt>$info</txt>"
     echo "<tool>$status_atual</tool>"
 }
