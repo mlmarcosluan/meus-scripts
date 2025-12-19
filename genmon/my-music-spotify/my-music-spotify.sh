09c328d0196ed139722c64d1361b557c5ae6be19
Marcos luan <mlmarcosluan@gmail.com>
2025-12-18 21:49:27 -0300

    Apenas organizando os códigos.

--- genmon/my-music-spotify/my-music-spotify.sh
+++ genmon/my-music-spotify/my-music-spotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-   ###   Função de verificação do status do spotify   ###
+   ###   Função de verificação do status do spotify
 get_spotify_status() {
     local status
     status=$(playerctl -p spotify status 2> /dev/null) # o 2> /dev/null cerve para esconder o erro caso tenha
@@ -15,16 +15,15 @@ get_spotify_status() {
     fi
 }
 
-   ### Função Main   ###
+   ### Função Main
 main() {
-
     # Variáveis locais
-    local info
     local status_atual
+    local info
     
     status_atual=$(get_spotify_status)
     # Se o status do spotify for diferente de "Em Reprodução" entra no segundo if, caso contrario sai do primeiro if sem fazer nada
-    if [[ "$status_atual" != "Em repordução" ]]; then # Se o $status_atual é igual a
+    if [[ "$status_atual" != "Em repordução" ]]; then
         if [[ "$status_atual" == "Pausado" ]]; then 
             echo "<txtclick>playerctl -p spotify play</txtclick><txt>$status_atual</txt>"
             echo "<tool>Spotify $status_atual</tool>"
@@ -33,9 +32,9 @@ main() {
         fi
         return
     fi
-
-    info=$(playerctl -p spotify metadata --format "{{title}} - {{artist}}")
     
+    info=$(playerctl -p spotify metadata --format "{{title}} - {{artist}}")
+
     # Saida para o genmon
     echo "<txtclick>playerctl -p spotify pause</txtclick><txt>$info</txt>"
     echo "<tool>$status_atual</tool>"
