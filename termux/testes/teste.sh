87f33c193431c2717c64468a47775e4a11950172
Marcos luan <mlmarcosluan@gmail.com>
2025-12-21 19:30:20 -0300

    Testando duas escolhas diferentes.

--- termux/testes/teste.sh
+++ termux/testes/teste.sh
@@ -2,9 +2,10 @@
 
 echo "Hello Word."
 
-escolha=$(termux-dialog radio -v "Video, Audio" -t "Selecione")
-
+escolha=$(termux-dialog radio -v "Video, Audio" -t "Selecione.")
+escolha2=$(termux-dialog radio -v "MP3, MP4" -t "Selecione a extenção do arquivo.")
 echo "$escolha"
+echo "$escolha2"
 
 read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
 echo ""
