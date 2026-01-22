404ae86112d291016ab8357451a5b5b677e07cd4
Marcos Luan <mlmarcosluan@gmail.com>
2026-01-22 01:36:58 -0300

    Adicionado uma função que verifica o estado dos dispositivos (0 = desativado, 1 = ativado ou "desconectado")

--- scripts/touch_mouse.sh
+++ scripts/touch_mouse.sh
@@ -13,11 +13,34 @@ id_dispositivos(){
     echo "${touch_id}|${mouse_id}"
 }
 
+estado_dispo(){
+    local id_touch=$1
+    local id_mouse=$2
+    local estado_touch
+    local estado_mouse
+
+    # Estado do Touchpad
+    estado_touch=$(xinput list-props $id_touch | grep "Device Enabled" | awk '{print $4}')
+
+    # Estado do Mouse
+    if [[ "$id_mouse" == "Desconectado" ]]; then
+        estado_mouse="Desconectado"
+    else
+        estado_mouse=$(xinput list-props $id_mouse | grep "Device Enabled" | awk '{print $4}')
+    fi
+
+    echo "${estado_touch}|${estado_mouse}"
+}
+
 main(){
     local touch_id
     local mouse_id
 
+    # Id dos dipositivos
     IFS="|" read -r touch_id mouse_id <<< "$(id_dispositivos)"
+
+    # Estado dos dispositivos
+    IFS="|" read -r estado_touch estado_mouse <<< "$(estado_dispo $touch_id $mouse_id)"
     
 }
 
