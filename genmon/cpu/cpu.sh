bedb16321a9fb9ecc68daa39875ebd0b100690f3
Marcos Luan <mlmarcosluan@gmail.com>
2026-02-04 13:06:42 -0300

    Organizado as funções

--- genmon/cpu/cpu.sh
+++ genmon/cpu/cpu.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 cal_uso_cpu(){
-# Ler os dados no /proc/stat
-read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
-total=$((user+nice+system+idle))
+    # Ler os dados no /proc/stat
+    read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
+    total=$((user+nice+system+idle))
 
-# Calcular total inicial e idle inicial
-prev_idle=$((idle + iowait))
-prev_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+    # Calcular total inicial e idle inicial
+    prev_idle=$((idle + iowait))
+    prev_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
 
-# Espera 1 segundo para calcular a taxa
-sleep 1
+    # Espera 1 segundo para calcular a taxa
+    sleep 1
 
-# Ler os dados FINAIS (segunda leitura)
-read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
+    # Ler os dados FINAIS (segunda leitura)
+    read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
 
-# Calcular total final e idle final
-curr_idle=$((idle + iowait))
-curr_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+    # Calcular total final e idle final
+    curr_idle=$((idle + iowait))
+    curr_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
 
-# Calcular a diferença dos dados coletados de uso
-diff_idle=$((curr_idle - prev_idle))
-diff_total=$((curr_total - prev_total))
+    # Calcular a diferença dos dados coletados de uso
+    diff_idle=$((curr_idle - prev_idle))
+    diff_total=$((curr_total - prev_total))
 
-# 5. Cálculo de uso
-cpu_usage=$(( (100 * (diff_total - diff_idle)) / (diff_total) ))
-printf -v cpu_usage_format "%3s" $cpu_usage
+    # 5. Cálculo de uso
+    cpu_usage=$(( (100 * (diff_total - diff_idle)) / (diff_total) ))
+    printf -v cpu_usage_format "%3s" $cpu_usage
 }
 
-
+cal_uso_cpu
 # Top processo
 top_procs=$(ps axch -o cmd:15,%cpu --sort=-%cpu | head -5 | awk '{printf "%s %s %% \n", $1, $2}')
 
