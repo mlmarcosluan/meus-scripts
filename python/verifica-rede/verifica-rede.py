a86a56bfaaf899caf3989afbfcef55b08a29efad
Marcos Luan <mlmarcosluan@gmail.com>
2026-03-23 16:13:43 -0300

    feat(net): adiciona a função "scanear(rede_alvo)" que escaneia a rede dada e retorna um dicionário com o ip e seu mac respectivamente

--- python/verifica-rede/verifica-rede.py
+++ python/verifica-rede/verifica-rede.py
@@ -28,14 +28,43 @@ def ip_rede():
     # 2. Transforma IP (ex.: 192.168.1.15 -> 192.168.1.0/24)
     rede_str = ip.rsplit(".", 1)[0] + ".0/24"
 
-    # Válida a rede com o ipaddress
+    # 3. Válida a rede com o ipaddress
     rede = ipaddress.ip_network(rede_str, strict=False)
 
-    return rede # Retorna a rede ja validada
+    return rede # Retorna um objeto IPv4Network
+
+def scanear(rede_alvo):
+    """
+    Escaneia a rede_alvo e retorna uma lista de ips de dispositivos conectados a rede
+    """
+    # 0. Converte a rede_alvo em texto puro
+    alvo_str = str(rede_alvo)
+
+    # 1. Cria o pacote com a camada fisica "sobre" a lógica
+    pacote = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=alvo_str)
+
+    # 2. Envia e recebe o pacote (timeout de 5 segundos)
+    respondidos, nao_respondidos = srp(pacote, timeout=5, verbose=False)
+
+    # 3. Extrai os dados dos pacotes recebidos diretamenta das variáveis internas do scapy e salva os em um dicionário
+    mapeamento_rede = {} # Dicionário vazio onde será adicionado os ip e macs
+    for pacote_enviado, pacote_recebido in respondidos:
+        ip_encontrado = pacote_recebido.psrc
+        mac_encontrado = pacote_recebido.hwsrc
+
+        # Adiciona ao dicionário
+        mapeamento_rede[ip_encontrado] = mac_encontrado
+    
+    return mapeamento_rede
+        
 
 def main():
     """Função principal, onde o script começa"""
-    pass
+    # Define a rede a ser análisada
+    rede = ip_rede()
+    mapeamento_rede = scanear(rede)
+
+    
 
 if __name__ == "__main__":
     try: # Adicionado um try/except para caso o script seja interrompido não tenha mensagem de erros
